1a80dc9066fed15a901979a1c049f41973607051
Javier Figueroa <javierfigueroa@Javiers-MacBook-Pro.local>
2024-05-12 10:10:56 -0600

    Modificaciones menu e inicio del juego

--- first_testing/eventos.c
+++ first_testing/eventos.c
@@ -1,57 +1,59 @@
-//*eventos.c
-//*librerías necesarias para las funciones
+//eventos.c
+//librerías necesarias para las funciones
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-//*archivo de implementación de la librería eventos.h.
+//archivo de implementación de la librería eventos.h.
 #include "eventos.h"
-//*Macros
-#define SRAND srand(time(NULL)); //*generar números aleatorios totalmente diferentes con el tiempo
+//Macros
+#define SRAND srand(time(NULL));//generar números aleatorios totalmente diferentes con el tiempo
+
+
 
 void inicializar_recursos(struct Recursos *recursos)
 {
     recursos->a = 0;
     recursos->c = 0;
 }
-//*funciones de eventos:
-void evento_caja_advertencia (struct Recursos *recursos) // *evento de caja con avisos
+
+
+//funciones de eventos:
+void evento_caja_advertencia (struct Recursos *recursos)//evento de caja con avisos
 {
     SRAND //macro
-    // * Generar un número aleatorio entre 0 y 10
+    // Generar un número aleatorio entre 0 y 10
     int probabilidad = rand() % 10;
 
-    printf("%d\n", probabilidad); //*linea no necesaria
-
-    //* Asignar los eventos con las probabilidades respectivas
-    if (probabilidad == 0 || probabilidad == 1 )//*probabilidad 20%
+    printf("%d\n", probabilidad);//linea no necesaria
+    // Asignar los eventos con las probabilidades respectivas
+    if(probabilidad == 0 || probabilidad == 1 )//probabilidad 20%
     {
         printf("Encontraste 3 unidades de comida\n");
         recursos->c = recursos->c + 3;
-        //*+3u comida
     } 
-    else if (probabilidad == 2) //*probabilidad 10%
+    else if (probabilidad == 2)//probabilidad 10%
     {
         printf("Encontraste 2 botellas de agua\n");
-        recursos->a = recursos->a + 3;
-        //*+2u de agua
+        recursos->a = recursos->a + 2;
+        //+2u de agua
     } 
-    else//*restante 70%
+    else//restante 70%
     {
         printf("Esta caja tenia dentro gases que te enfermaron\n");
         recursos->c = recursos->c - 2;
         recursos->a = recursos->a - 3;
-        //*-2u de comida y -3u agua
     }
 }
 
-void evento_caja (struct Recursos *recursos) //*evento de caja sola
-{
-    SRAND //*macro
-    int probabilidad = rand() % 2;
-    printf("%d\n", probabilidad);//*linea no necesaria
 
-    if (probabilidad == 0 )//*probabilidad 50%
+
+void evento_caja (struct Recursos *recursos) //evento de caja sola
+{
+    SRAND //macro
+    int probabilidad = rand() % 2;
+    printf("%d\n", probabilidad);//linea no necesaria
+    if (probabilidad == 0 )//probabilidad 50%
     {
         printf("Encontraste 2 unidades de comida\n");
         recursos->c = recursos->c + 2;
@@ -59,9 +61,46 @@ void evento_caja (struct Recursos *recursos) //*evento de caja sola
     } 
     else //probabilidad 50%
     {
-        printf("Una ratón te robo, perdiste 2 unidades de comida\n");
+        printf("Un ratón te robo, perdiste 2 unidades de comida\n");
         recursos->c = recursos->c - 2;
-        //-2u de comida
     } 
-    
+    //recursos-> dia = recursos-> dia + 0.5;
+}
+
+
+//Asignar dicha probabilidad dentro de eventos
+void probabilidad_eventos (struct Recursos *recursos)
+{
+    SRAND//macro
+    int probabilidad = rand()%10;
+    if (probabilidad == 0 || probabilidad == 1)  
+    {
+        //función de la caja normal
+        printf("Acabas de encontrar una caja normal\n");
+        printf("Quieres abrir la caja? 1 para si, 0 para no: ");
+        int abrirCaja;
+        scanf("%d", &abrirCaja);
+        if (abrirCaja == 1)
+        {
+            evento_caja(recursos);
+        }
+        
+    }
+    else if (probabilidad == 2)//evento caja con avisos
+    {
+        printf("Acabas de encontrar una caja con varias etiquetas de advertencia\n");
+        printf("Quieres abrir la caja? 1 para si, 0 para no: ");
+        int abrirCaja;
+        scanf("%d", &abrirCaja);
+        if (abrirCaja == 1)
+        {
+            evento_caja_advertencia(recursos);
+        }
+    }
+    //las demás futuras funciones
+    else
+    {
+        printf("Coming soon(Próximamente más funciones)");
+    }
+
 }
