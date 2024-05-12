fc30d3e9462360ba510fba5c2ebb6ba9f3dd9b08
alan80avalos <alejando.rodriguez@iteso.mx>
2024-05-12 13:22:40 -0600

    cambio de probabilidad de eventos de hayer

--- first_testing/main.c
+++ first_testing/main.c
@@ -1,13 +1,13 @@
-//*main.c
+//main.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-//librerías propias:
+//librerias propias:
 #include "eventos.h"
 //Macros
-#define SRAND srand(time(NULL));//generar números aleatorios totalmente diferentes con el tiempo
+#define SRAND srand(time(NULL));//generar numeros aleatorios totalmente diferentes con el tiempo
 
-//* Funciones:
+//funciones:
 void eventos(); 
 void menu_inicial();
 void introduccion();
@@ -21,28 +21,20 @@ int main ()
     return 0;
 }
 
-//*Función que imprime el menu inicial con 2 simples opciones
+//Funcion que imprime el menu incial con 2 simples opciones
 void menu_inicial ()
 {
     int menu;
-    printf("1. Jugar\n2. Créditos\n");
-    printf("Elige una opción para comenzar: ");
+    printf("1. Jugar\n2. Creditos\n");
+    printf("Elige una opcion para comenzar: ");
     scanf("%d",&menu);
     switch (menu)
     {
     case 1:
         introduccion();
-
-        //* Estas 3 lineas de código solo son de prueba para ver que funcionan correctamente las librerías
-        printf("Quieres abrir la caja? -1 para si, -0 para no: ");
-        int abrirCaja;
-        scanf("%d", &abrirCaja);
-        if (abrirCaja == 1)
-        {
-            struct Recursos recursos; // Crear una instancia de Recursos
-            inicializar_recursos(&recursos); // Inicializar la estructura
-            evento_caja(&recursos); // Pasar la estructura a la función
-        }
+        struct Recursos recursos;
+        inicializar_recursos(&recursos);
+        probabilidad_eventos(&recursos);
         break;
     case 2:
         creditos();
@@ -56,13 +48,13 @@ void menu_inicial ()
 void introduccion ()
 {
     printf("-Hola usuario actualmente estas en un apocalipsis zombie, en donde debes aplicar lo que sabes sobre supervivencia para avanzar.\n");
-    printf("-Recuerda que tus acciones pueden traerte tanto beneficios como también consecuencias\n\n\n");
+    printf("-Recuerda que tus acciones pueden traerte beneficios como tambien consecuencias\n");
     
 }
 
 void creditos ()
 {
-    printf("Gracias por jugar atte:\n-Alan\n-Victoria\n-Javier\n");
+    printf("Gracias por jugar att:\n-Alan\n-Victoria\n-Francisco\n");
 }
 void inventario ()
 {
